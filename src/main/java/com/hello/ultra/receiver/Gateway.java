a0ac9650f2ab413ae9d3af4dea7b77ce48c05e8b
권용근 <kingbbode@gmail.com>
2017-02-12 18:07:12 +0900

    convert방식에서 PathVariable 방식으로 변

--- src/main/java/com/hello/ultra/receiver/Gateway.java
+++ src/main/java/com/hello/ultra/receiver/Gateway.java
@@ -3,7 +3,6 @@ package com.hello.ultra.receiver;
 import com.hello.ultra.base.MessengerFactory;
 import com.hello.ultra.base.enums.API;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,18 +13,9 @@ public class Gateway {
     @Autowired
     private MessengerFactory messengerFactory;
 
-
-    public Object proceed(HttpRequest httpRequest, Object object){
-        return proceed(resolve(httpRequest), object);
-    }
-
     public Object proceed(API api, Object object){
         return messengerFactory
                 .get(api)
                 .proceed(object);
-    }
-
-    private API resolve(HttpRequest httpRequest){
-        return null;
     }
 }
--- src/main/java/com/hello/ultra/receiver/controller/MessageController.java
+++ src/main/java/com/hello/ultra/receiver/controller/MessageController.java
@@ -1,8 +1,10 @@
 package com.hello.ultra.receiver.controller;
 
+import com.hello.ultra.base.enums.API;
 import com.hello.ultra.receiver.Gateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +18,8 @@ public class MessageController {
     @Autowired
     private Gateway gateway;
 
-    @PostMapping("/message")
-    public Object message(HttpRequest httpRequest, @RequestParam Object data) {
-        return gateway.proceed(httpRequest, data);
+    @PostMapping("{api}/message")
+    public Object message(@PathVariable API api, @RequestParam Object data) {
+        return gateway.proceed(api, data);
     }
 }
